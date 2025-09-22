fe7b0bf8340e6967c2a0bea2459b31dde63a2a61
Nana Sarfo <sarfonanayaw11@gmail.com>
2025-09-22 17:19:21 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+const menuicon = document.querySelector('.menu_icon');
+const menuiconicon = document.querySelector('.menu_icon i');
+const dropdown = document.querySelector('.dropdown');
+
+menu_icon.oneclick=function(){
+    dropdown.classList.menu_icon('open')
+}
